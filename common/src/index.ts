4da83c3909e35084db8dd1c769fb78a2e118bd55
Ranjan N <ranjann2004@gmail.com>
2024-11-10 13:18:51 +0530

    Added common for type inferance using zod

---
+++ common/src/index.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+
+export const signUpSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().optional(),
+});
+
+export type SignUpInput = z.infer<typeof signUpSchema>;
+
+export const signInSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+
+export type SignInInput = z.infer<typeof signInSchema>;
+
+export const createBlogSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export type CreateBlogInput = z.infer<typeof createBlogSchema>;
+
+export const updateBlogSchema = z.object({
+  title: z.string(),
+  content: z.string(),
+  id: z.string(),
+});
+
+export type UpdateBlogInput = z.infer<typeof updateBlogSchema>;
