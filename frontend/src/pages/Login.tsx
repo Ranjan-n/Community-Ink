6b987f0e39936a87373a4db515fb6b666e5cfdf4
Ranjan N <ranjann2004@gmail.com>
2024-11-17 13:51:51 +0530

    Updated Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { SignIn } from "../components/SignIn";
 import { SignUp } from "../components/SignUp";
 
 export function Login() {
+  const [showSignIn, setShowSignIn] = useState(false);
+
   return (
-    <div className="flex justify-around items-center h-screen">
-      <SignUp />
-      <SignIn />
-    </div>
+    <>
+      {showSignIn ? (
+        <SignIn setShowSignIn={setShowSignIn} showSignIn={showSignIn} />
+      ) : (
+        <SignUp setShowSignIn={setShowSignIn} showSignIn={showSignIn} />
+      )}
+    </>
   );
 }
