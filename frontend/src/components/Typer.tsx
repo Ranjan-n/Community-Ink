aee345d75b60758ab9241a3f8bab8bad5bbf0b1b
Ranjan N <ranjann2004@gmail.com>
2024-11-18 19:23:48 +0530

    Added typer component for login page

---
+++ frontend/src/components/Typer.tsx
@@ -0,0 +1,29 @@
+import Typewriter from "typewriter-effect";
+
+export function Typer({
+  title,
+  content,
+}: {
+  title: string;
+  content: string[];
+}) {
+  return (
+    <div className="w-1/2 flex flex-col justify-center items-center bg-gradient-to-r from-violet-300 to-red-300">
+      <h2 className="text-4xl font-black text-white tracking-tight [text-shadow:_0_2px_6px_rgba(0,0,0,0.25)] mb-4">
+        {title}
+      </h2>
+      <p className="text-xl font-bold text-white [text-shadow:_0_1px_2px_rgba(0,0,0,0.2)] mb-8">
+        <Typewriter
+          options={{
+            strings: content,
+            autoStart: true,
+            loop: true,
+            delay: 10,
+            deleteSpeed: 10,
+            cursor: "_",
+          }}
+        />
+      </p>
+    </div>
+  );
+}
